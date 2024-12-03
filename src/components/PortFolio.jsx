61c9de4786041f5a0afa0e8cdc33429c9ba40ec2
Nagraj Chitme <161920374+nagrajchitme@users.noreply.github.com>
2024-12-03 11:43:53 +0530

    Update PortFolio.jsx

--- src/components/PortFolio.jsx
+++ src/components/PortFolio.jsx
@@ -2,41 +2,44 @@ import React from "react";
 import java from "../../public/java.png";
 import python from "../../public/python.webp";
 import mongoDB from "../../public/mongodb.jpg";
-import express from "../../public/express.png";
 import reactjs from "../../public/reactjs.png";
 import nodejs from "../../public/node.png";
+import MYSQL from "../../public/mysql.png";
+
 function PortFolio() {
   const cardItem = [
+    
     {
       id: 1,
-      logo: mongoDB,
-      name: "MongoDB",
-    },
-    {
-      id: 2,
-      logo: express,
-      name: "Express",
-    },
-    {
-      id: 3,
       logo: reactjs,
       name: "ReactJS",
     },
     {
-      id: 4,
+      id: 2,
       logo: nodejs,
       name: "NodeJS",
     },
     {
-      id: 5,
+      id: 3,
+      logo: java,
+      name: "Java",
+    },
+    {
+      id: 4,
       logo: python,
       name: "Python",
     },
     {
-      id: 6,
-      logo: java,
-      name: "Java",
+      id: 5,
+      logo: mongoDB,
+      name: "MongoDB",
     },
+    {
+      id: 7,
+      logo: MYSQL,
+      name: "MYSQL",
+    },
+    
   ];
   return (
     <div
@@ -60,8 +63,14 @@ function PortFolio() {
               <div>
                 <div className="px-2 font-bold text-xl mb-2">{name}</div>
                 <p className="px-2 text-gray-700">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                projects are added in git hubif you want can check it.
                 </p>
+
+                <p className="px-2 text-gray-700">
+                
+                </p>
+
+
               </div>
               <div className=" px-6 py-4 space-x-3 justify-around">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded">
