2b3bb00bb5de934374d0e533bc147f557772e583
Nagraj Chitme <161920374+nagrajchitme@users.noreply.github.com>
2024-12-03 11:39:12 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,8 +9,8 @@ function About() {
       <div>
         <h1 className="text-3xl font-bold mb-5">About</h1>
         <p>
-          Hello, I'm Akhil, a passionate Web developer with a keen eye for MERN
-          Stack . With a background in IT, I strive to create impactful and
+          Hello, I'm Nagraj Chitme, a passionate Web developer with a keen eye for MERN
+          Stack . java full stack Developer. With a background in IT, I strive to create impactful and
           visually stunning Software solutions that leave a lasting impression.
         </p>
         <br />
@@ -18,9 +18,11 @@ function About() {
           Education & Training
         </h1>
         <span>
-          [Degree/Certification], [Institution], [Year] [Degree/Certification],
-          [Institution], [Year] [Relevant Course], [Platform/Institution],
-          [Year]
+          
+          - Secondary School Leaving Certificate (SSLC), KSSEEB (2015) - 70.00%<br/>
+          - Pre-University Certificate (PUC), Department of Pre-University Education (2018) - 55.00%<br/>
+          - Bachelor of Science ((Life Science)), Gulbarga University, Kalaburagi (2022) - 60.00%<br/>
+          - Master of Computer Applications (MCA), Visvesvaraya Technological University, Regional Campus, Kalaburagi (2024) - 83.00%
         </span>
         <br />
         <br />
@@ -28,22 +30,93 @@ function About() {
           Skills & Expertise
         </h1>
         <span>
-          Proficient in [Programming Languages] Experienced with [Software
-          Tools/Technologies] Strong grasp of [Design Principles/Concepts]
-          Excellent problem-solving skills Effective communicator and
-          collaborator
+
+          <h4 className="text-green-800 font-semibold text-xl">
+          Basic Computer Skills
+        </h4>
+
+
+
+<li>Microsoft Office</li> 
+<li>PowerPoint</li> 
+<li>Excel</li>
+
+<h4 className="text-green-800 font-semibold text-xl">
+          Technical Skills
+        </h4>
+
+
+<ul><u>Front-end Development</u></ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>JavaScript</li>
+<li>React.js</li>
+<li>Node.js</li>
+
+<ul><u>Back-end Development</u></ul>
+<li>Java (Advanced)</li>
+<li>Python</li>
+<li>.NET Framework</li>
+<li>Cloud</li>
+<li>Spring Boot</li>
+<li>Hibernate</li>
+<li>JDBC</li>
+
+<ul><u>Database Management</u></ul>
+<li>MongoDB</li>
+<li>MySQL</li>
+<li>Oracle</li> 
         </span>
         <br />
         <br />
         <h1 className="text-green-600 font-semibold text-xl">
-          Professional Experience
+        Professional Experience
         </h1>
+        <br/>
         <span>
-          [Job Title], [Company/Organization], [Dates] [Brief description of
-          responsibilities and achievements] [Job Title],
-          [Company/Organization], [Dates] [Brief description of responsibilities
-          and achievements] [Freelance/Contract Work], [Client/Organization],
-          [Dates] [Brief description of projects and contributions]
+
+          <ul>Job title:- Sales Associate</ul> 
+          <ul>Company name:- FirstCry</ul>
+          <ul>Dates of employment:-</ul>
+          <ul>Brief description of job responsibilities: 
+          - Provide exceptional customer service, responding to customer inquiries and resolving issues in a timely and professional manner.
+          - Maintain a high level of product knowledge, staying up-to-date on new products, promotions, and company initiatives.
+          - Drive sales growth by meeting or exceeding sales targets, promoting products, and suggesting additional items to customers.
+          - Maintain a clean and organized store environment, including visual merchandising and inventory management.
+          - Collaborate with colleagues to achieve sales goals, share product knowledge, and provide support as needed.
+          - Participate in visual merchandising, inventory management, and other sales floor activities as required.
+          - Stay informed about company policies, procedures, and promotions, and communicate this information to customers and colleagues.
+          </ul>
+
+
+          {/* <li>WHAT’S APP CHAT ANALYSIS (PYTHON)<br/>
+          The WhatsApp chat Analysis project provides a comprehensive platform where users can upload and analyze their
+          WhatsApp conversation be it individual or group chats</li>
+          <br/>
+          <li>DATA INTEGRITY AUDIT SCHEME BASED ON QUAD MERKLE AND TREE (JAVA)<br/>
+          User can store their data safe and securely without having fear of lost and corrupt the datastores in blocks</li>
+          <br/>
+          <li>PORTFOLIO<br/>
+          Using HTML CSS and java Script node.js and React.js made attractive portfolio</li>
+          <br/>
+          <li>Simple Website<br/>
+          Using HTML CSS and java Script with dynamic and attract Butten using hover and dynamic login page</li>
+          <br/>
+          <li>BANKING PROJECT (JAVA)<br/>
+          User can create their account save the money withdraw amount and check Balance. here user safe and securely keep
+          their private information</li>
+          <br/>
+          <li>SMART RAILWAY GATE (IOT)<br/>
+          Using Raspberry Pi Sencers male female wire and model we made smart railway Gate if train coming then it will close
+          the gate and won’t allow to move forward </li>
+          <br/>
+          <li>RETAILER SHOP WEBSITE (JAVA)<br/>
+          Here customer can resister them self and order the products and pay the amount for home delivery cart items</li>
+          <br/>
+          <li>CLONE MUSIC APPLICATION<br/>
+          Using HTML CSS and java Script here user add songs play stop change song forward and backward the musi</li> */}
+
+
         </span>
         <br />
         <br />
@@ -51,8 +124,8 @@ function About() {
           Achievements & Awards
         </h1>
         <span>
-          [Award/Recognition], [Organization/Institution], [Year] [Achievement],
-          [Organization/Platform], [Year]
+          <li>Introduction to Artificial Intelligence on October 14, 2024</li>
+          <li>For successful completion of internship on “Machine Learning” with Grade “A” from 27/10/2023 to 09/12/2023</li>
         </span>
         <br />
         <br />
@@ -60,11 +133,7 @@ function About() {
           Mission Statement
         </h1>
         <p>
-          My mission is to leverage my skills and creativity to deliver
-          innovative [Your Field] solutions that exceed client expectations and
-          contribute positively to the digital landscape. I am committed to
-          continuous learning and growth, always seeking new challenges and
-          opportunities to expand my horizons.
+        To become a leading expert in full-stack development, leveraging expertise in Java, Python, and JavaScript. Design, develop, and deploy scalable, efficient, and innovative software solutions that drive business growth and success. Utilize expertise to analyze complex problems, develop effective solutions, and collaborate with cross-functional teams. Continuously update skills to stay current with industry trends, delivering high-quality solutions that exceed expectations and contribute to business success.
         </p>
       </div>
     </div>
