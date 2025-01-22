8cdea60490a4590ddb9017607191239dff897f78
Nagraj Chitme <161920374+nagrajchitme@users.noreply.github.com>
2025-01-22 20:20:06 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import {
-  FaFacebook,
-  FaTwitter,
-  FaInstagram,
-  FaLinkedinIn,
-} from "react-icons/fa6";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
 function Footer() {
   return (
     <>
@@ -12,16 +10,44 @@ function Footer() {
       <footer className="py-12">
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
           <div className=" flex flex-col items-center justify-center">
-            <div className="flex space-x-4">
-              <FaFacebook size={24} />
-              <FaTwitter size={24} />
-              <FaInstagram size={24} />
-              <FaLinkedinIn size={24} />
+            <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
+              <div className="  space-y-2">
+                <h1 className="font-bold text-center ">Available on</h1>
+                <ul className="flex space-x-5">
+                  <li>
+                    <a href="https://www.facebook.com/" target="_blank">
+                      <FaSquareFacebook className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/nagrajchitme"
+                      target="_blank"
+                    >
+                      <FaLinkedin className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/rajvchitme"
+                      target="_blank"
+                    >
+                      <FaInstagram className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.Telegram.com/nagrajchitme"
+                      target="_blank"
+                    >
+                      <FaTelegram className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
-              <p className="text-sm">
-                
-              </p>
+              <p className="text-sm"></p>
               <p className="text-sm"> ❤️ </p>
             </div>
           </div>
